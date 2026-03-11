62114448ec0de09d896c3f92489908cd935cbafe
sbridewell <simon@sbridewell.me.uk>
2026-03-11 19:08:53 +0000

    Initial implementation of Newton's method for finding roots of a function.

---
+++ NeuralNetworks/TrainingDataProviders/NewtonsMethod.cs
@@ -0,0 +1,69 @@
+﻿// <copyright file="NewtonsMethod.cs" company="Simon Bridewell">
+// Copyright (c) Simon Bridewell. All rights reserved.
+// </copyright>
+
+namespace Sde.NeuralNetworks.TrainingDataProviders
+{
+    /// <summary>
+    /// Implementation of Newton's method for finding roots of a function.
+    /// </summary>
+    public class NewtonsMethod
+    {
+        /// <summary>
+        /// Finds a root of the given function using Newton's method.
+        /// </summary>
+        /// <param name="functionToFindRootOf">
+        /// The function to find the root of.
+        /// </param>
+        /// <param name="derivativeOfFunction">
+        /// The derivative function of <see paramref="functionToFindRootOf"/>.
+        /// </param>
+        /// <param name="initialGuess">
+        /// An arbitrary initial guess for the root.
+        /// The closer this is to the actual root, the faster the method will converge.
+        /// </param>
+        /// <param name="tolerance">
+        /// The method will return once the absolute value of the function at the current
+        /// guess is less than this value.
+        /// </param>
+        /// <param name="epsilon">
+        /// If the derivative function returns less than this value, the method will
+        /// throw an exception to avoid division by zero.
+        /// </param>
+        /// <param name="maxIterations">
+        /// The maximum number of iterations to perform before giving up and throwing an
+        /// exception.
+        /// </param>
+        /// <returns>
+        /// A root of the function, or an approximation of a root if the method converges.
+        /// </returns>
+        public static double FindRoot(
+            Func<double, double> functionToFindRootOf,
+            Func<double, double> derivativeOfFunction,
+            double initialGuess,
+            double tolerance = 1e-7,
+            double epsilon = 1e-10,
+            int maxIterations = 1000)
+        {
+            var x = initialGuess;
+            for (var i = 0; i < maxIterations; i++)
+            {
+                var fx = functionToFindRootOf(x);
+                if (Math.Abs(fx) < tolerance)
+                {
+                    return x; // Found a root
+                }
+
+                var fpx = derivativeOfFunction(x);
+                if (fpx < epsilon)
+                {
+                    throw new InvalidOperationException("Derivative is too close to zero. No solution found.");
+                }
+
+                x = x - (fx / fpx); // Update guess using Newton's method formula
+            }
+
+            throw new InvalidOperationException("Maximum iterations reached. No solution found.");
+        }
+    }
+}
